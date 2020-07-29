6da40bad4b87600e0cc5d5668c68c51185b8b4c8
oriyanh <oriyanh@cs.huji.ac.il>
2020-07-29 18:58:31 +0300

    Added skeleton for new player object

--- players.py
+++ players.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from enum import Enum
 
 PlayersEnum = Enum("PlayersEnum", ['N', 'E', 'S', 'W'])
@@ -6,6 +7,20 @@ PlayersEnum = Enum("PlayersEnum", ['N', 'E', 'S', 'W'])
 class Player:
     def __init__(self, p: PlayersEnum):
         self.p = p
+
+    def __str__(self):
+        return self.p.name
+
+class PlayerNew:
+    def __init__(self, p: PlayersEnum):
+        self.p = p
+        self.hand = None
+        self.direction = None
+
+    def play_card(self):
+        pass
+    def get_legal_actions(self, state):
+        pass
 
     def __str__(self):
         return self.p.name
