dbb326b0f88c20b32fda0b3cd3a48dee12f9ad85
oriyanh <oriyanh@cs.huji.ac.il>
2020-07-30 19:15:32 +0300

    Enhanced Player class

--- players.py
+++ players.py
@@ -1,32 +1,66 @@
-from abc import ABC, abstractmethod
 from enum import Enum
+from typing import List
 
-PlayersEnum = Enum("PlayersEnum", ['N', 'E', 'S', 'W'])
+from cards import Hand, Card
 
+
+PositionEnum = Enum("PlayersEnum", ['N', 'E', 'S', 'W'])
+
+POSITIONS = list(PositionEnum)
+
+TEAMS = [(PositionEnum.N, PositionEnum.S),
+         (PositionEnum.W, PositionEnum.E)]
 
 class Player:
-    def __init__(self, p: PlayersEnum):
-        self.p = p
 
-    def __str__(self):
-        return self.p.name
+    def __init__(self, position: PositionEnum, hand: Hand):
+        self.position = position
+        self.hand = hand
 
-class PlayerNew:
-    def __init__(self, p: PlayersEnum):
-        self.p = p
-        self.hand = None
-        self.direction = None
+    def play_card(self, card: Card):
+        self.hand.play_card(card)
 
-    def play_card(self):
-        pass
-    def get_legal_actions(self, state):
+    def get_legal_actions(self, trick):
         pass
 
     def __str__(self):
-        return self.p.name
+        return self.position.name
 
+    def __eq__(self, other):
+        return self.position == other.position
 
-PLAYERS = [Player(PlayersEnum.N),
-           Player(PlayersEnum.E),
-           Player(PlayersEnum.S),
-           Player(PlayersEnum.W)]
+    def __hash__(self) -> int:
+        return hash(self.position)
+
+class Team:
+
+    def __init__(self, p0: Player, p1: Player):
+        self.players = [p0, p1]
+        self.teammate = {p0.position: p1, p1.position: p0}
+
+    def __str__(self):
+        return f"{self.players[0]}{self.players[1]}"
+
+    def has_player(self, p: Player) -> bool:
+        """
+
+        :param p:
+        :return:
+        """
+        return p in self.players
+
+    def get_players(self) -> List[Player]:
+        """
+
+        :return:
+        """
+        return self.players
+
+    def get_teammate(self, p: Player) -> Player:  # todo [ORIYAN] Possibly remove?
+        """
+
+        :param p:
+        :return:
+        """
+        assert (p in self.players)
+        return self.teammate[p.position]
