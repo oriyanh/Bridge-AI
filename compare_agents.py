8e99d1bc484a86f3b3aef779648719c03fd40d8f
marchellyn <marynar@cs.huji.ac.il>
2020-08-11 16:26:14 +0300

    #3 graph creation

--- compare_agents.py
+++ compare_agents.py
@@ -164,8 +164,8 @@ def compare_simple_agents_vs_ab_agents(depth, hand_size):
 
 
 # compare_simple_agents()
-compare_simple_agents_vs_ab_agents(depth=5, hand_size=4)
-compare_simple_agents_vs_ab_agents(depth=10, hand_size=4)
+# compare_simple_agents_vs_ab_agents(depth=5, hand_size=4)
+# compare_simple_agents_vs_ab_agents(depth=10, hand_size=4)
 compare_simple_agents_vs_ab_agents(depth=15, hand_size=4)
 compare_simple_agents_vs_ab_agents(depth=5, hand_size=8)
 compare_simple_agents_vs_ab_agents(depth=10, hand_size=8)
