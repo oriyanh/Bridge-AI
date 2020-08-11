45739879e3d6963dbc86570ea2de77156d094d3f
oriyanh <oriyanh@cs.huji.ac.il>
2020-08-11 16:03:19 +0300

    Fixed compare agents so I can run it from CLI with arguments

--- compare_agents.py
+++ compare_agents.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 
 from match import *
 
-GAMES_PER_MATCH = 100
 
 simple_func_names = [
     'highest_first_action',
@@ -37,6 +36,10 @@ ab_evaluation_agent_names = ['ShortGreedyEvaluation',
                              'LongGreedyEvaluation',
                              'HandEvaluation',
                              'CountOfTricksWon']
+
+NUM_CARDS = 13
+GAMES_PER_MATCH = 100
+
 
 
 def compare_simple_agents():
@@ -113,12 +116,12 @@ def compare_simple_agents_vs_ab_agents(depth, ab_first=True):
                     curr_match = Match(ab_agent, simple_agent,
                                        num_games=GAMES_PER_MATCH,
                                        verbose_mode=False,
-                                       cards_in_hand=5)
+                                       cards_in_hand=NUM_CARDS)
                 else:
                     curr_match = Match(simple_agent, ab_agent,
                                        num_games=GAMES_PER_MATCH,
                                        verbose_mode=False,
-                                       cards_in_hand=5)
+                                       cards_in_hand=NUM_CARDS)
 
                 curr_match.run()
                 print(f"Score: {curr_match.games_counter[0]:02} -"
@@ -169,21 +172,9 @@ def compare_simple_agents_vs_ab_agents(depth, ab_first=True):
           f"{int(time.time() - start_time)} seconds ---")
 
 def run_all_simple_agents_vs_mcts(agent_cls, num_cards=13, **kwargs):
-    results = np.empty((len(ab_evaluation_func_names),
+    results = np.empty((len(simple_func_names),
                         len(simple_func_names)))
     results[:] = np.nan
-    for i in range(len(simple_func_names)):
-        for j in range(len(simple_func_names)):
-            agent0 = simple_func_names[i]
-            print(f"{simple_agent_names[i]} vs. {agent_cls.__name__} ({simple_func_names[j]} rule)")
-            curr_match = Match(SimpleAgent(agent0),
-                               agent_cls(simple_func_names[j], **kwargs),
-                               GAMES_PER_MATCH, False, cards_in_hand=num_cards)
-            curr_match.run()
-            print(f"Score: {curr_match.games_counter[0]:02} -"
-                  f" {curr_match.games_counter[1]:02}\n")
-            results[j, i] = 100 * curr_match.games_counter[1] / GAMES_PER_MATCH
-    display_table_simple_agents_vs_MCTS(False, agent_cls, **kwargs)
     for i in range(len(simple_func_names)):
         for j in range(len(simple_func_names)):
             agent0 = simple_func_names[i]
@@ -195,18 +186,16 @@ def run_all_simple_agents_vs_mcts(agent_cls, num_cards=13, **kwargs):
             print(f"Score: {curr_match.games_counter[0]:02} -"
                   f" {curr_match.games_counter[1]:02}\n")
             results[j, i] = 100 * curr_match.games_counter[0] / GAMES_PER_MATCH
-    display_table_simple_agents_vs_MCTS(results, True, agent_cls, num_cards=num_cards, **kwargs)
+    display_table_simple_agents_vs_MCTS(results, agent_cls, num_cards=num_cards, **kwargs)
 
-def display_table_simple_agents_vs_MCTS(results, mcts_first, agent_cls, num_simulations=1, epsilon=None, num_cards=13):
+def display_table_simple_agents_vs_MCTS(results, agent_cls, num_simulations=1, epsilon=None, num_cards=13):
     fig, ax = plt.subplots(dpi=300)
     im = ax.imshow(results, cmap='plasma', vmin=0, vmax=100)
     ax.set_xticks(np.arange(len(simple_agent_names)))
     ax.set_yticks(np.arange(len(simple_agent_names)))
     mcts_agent_names = [f"{agent_cls.__name__}({name})" for name in simple_agent_names]
-    if mcts_first:
-        title = f"Win % of {agent_cls.__name__} \nvs normal agent - MCTS first, {num_simulations} sims."
-    else:
-        title = f"Win % of {agent_cls.__name__} \nvs normal agent - normal agent first, {num_simulations} sims."
+    title = f"Win % of {agent_cls.__name__} vs normal agent\n" \
+            f"{NUM_GAMES} games, {num_cards} cards, {num_simulations} sims."
     ax.set_title(title, fontsize=12, fontweight="bold")
     ax.set_xticklabels(simple_agent_names, fontsize=8)
     ax.set_yticklabels(mcts_agent_names, fontsize=8)
@@ -223,29 +212,50 @@ def display_table_simple_agents_vs_MCTS(results, mcts_first, agent_cls, num_simu
     fig.tight_layout()
     plot_dir = os.path.join(os.getcwd(), 'plots')
     os.makedirs(plot_dir, exist_ok=True)
-    plot_fname = os.path.join(plot_dir, f"{agent_cls.__name__}_{'1st' if mcts_first else '2nd'}"
+    plot_fname = os.path.join(plot_dir, f"{agent_cls.__name__}_"
                                         f"_{NUM_GAMES}games_{num_cards}cards_{num_simulations}sims"
                                         f"{f'_{epsilon:1.2}eps' if epsilon else ''}.jpg")
-    plt.fill()
+    plt.tight_layout()
     plt.savefig(plot_fname)
     plt.show()
 
+LEGAL_AGENT_TYPES =["AB", "MCTS", "SIMPLE"]
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('--agent', required=True, type=str, choices=LEGAL_AGENT_TYPES, dest='agent')
+    parser.add_argument('--games', default=100, type=int)
+    parser.add_argument('--simulations', default=1000, type=int)
+    parser.add_argument('--cards', default=5, type=int)
+    parser.add_argument('--depth', default=5, type=int)
+    parser.add_argument('--epsilon', default=0., type=float)
+    args = parser.parse_args()
+    return args
 
+def mcts_main(num_games, num_simulations, num_cards, epsilon):
+    global GAMES_PER_MATCH
+    GAMES_PER_MATCH = num_games
+    MCTS_classes = [SimpleMCTSAgent, PureMCTSAgent]
+    for agent_cls in MCTS_classes:
+        run_all_simple_agents_vs_mcts(agent_cls, num_simulations=num_simulations, num_cards=num_cards)
+
+    run_all_simple_agents_vs_mcts(StochasticSimpleMCTSAgent, num_simulations=num_simulations, num_cards=num_cards, epsilon=epsilon)
+
+def ab_main(num_games, num_cards, depth):
+    global GAMES_PER_MATCH, NUM_CARDS
+    GAMES_PER_MATCH = num_games
+    NUM_CARDS = num_cards
+    compare_simple_agents_vs_ab_agents(depth=5, ab_first=True)
+    compare_simple_agents_vs_ab_agents(depth=5, ab_first=False)
+    compare_simple_agents_vs_ab_agents(depth=10, ab_first=True)
+    compare_simple_agents_vs_ab_agents(depth=10, ab_first=False)
+    compare_simple_agents_vs_ab_agents(depth=15, ab_first=True)
+    compare_simple_agents_vs_ab_agents(depth=15, ab_first=False)
 
 if __name__ == '__main__':
-    # compare_simple_agents_vs_ab_agents(depth=5, ab_first=True)
-    # compare_simple_agents_vs_ab_agents(depth=5, ab_first=False)
-    # compare_simple_agents_vs_ab_agents(depth=10, ab_first=True)
-    # compare_simple_agents_vs_ab_agents(depth=10, ab_first=False)
-    # compare_simple_agents_vs_ab_agents(depth=15, ab_first=True)
-    # compare_simple_agents_vs_ab_agents(depth=15, ab_first=False)
-
-    # run_all_simple_actions_matches()
-    # display_table_simple_agents()
-    num_simulations = 1000
-    num_cards = 7
-    # run_all_simple_agents_vs_mcts(SimpleMCTSAgent, num_simulations=num_simulations)
-    # run_all_simple_agents_vs_mcts(StochasticSimpleMCTSAgent, num_simulations=num_simulations, epsilon=0.2)
-    run_all_simple_agents_vs_mcts(PureMCTSAgent, num_simulations=num_simulations, num_cards=num_cards)
-    # run_all_simple_agents_vs_stochastic_mcts(, num_simulations, 0.2)
-
+    args = parse_args()
+    if args.agent.upper() == 'MCTS':
+        mcts_main(args.games, args.simulations, args.cards, args.epsilon)
+    if args.agent.upper() == 'AB':
+        ab_main(args.games, args.cards, args.depth)
+    if args.agent.upper() == 'SIMPLE':
+        compare_simple_agents()
